988850df04f94856a271086313574eee13691685
franlu0 <162721582+franlu0@users.noreply.github.com>
2024-05-23 12:59:57 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,14 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    x=[]
+    for e in lista:
+        if e != "":
+            x+=  [f"{len (x)}. {e}"]  
+    return x
 
-
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista):
+    x=[]
+    for e in lista:
+        if e != "":
+            x+=  [f"{len (x)}. {e[::-1]}"]  
+    return x
+    
